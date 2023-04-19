2852183d349e6ac218f7ac7f1a213407e4012ec2
Core <64542347+coredev-uk@users.noreply.github.com>
2023-04-19 08:52:58 +0100

    ported over

---
+++ MusicKit.Events.d.ts
@@ -0,0 +1,142 @@
+declare namespace MusicKit {
+  /**
+   * A dictionary containing events for a MusicKit instance.
+   */
+  interface Events {
+    /**
+     * A notification name indicating a change in the authorization status.
+     */
+    authorizationStatusDidChange: { authorizationStatus: AuthStatus[keyof AuthStatus] };
+    /**
+     * A notification name indicating an upcoming change in the authorization status.
+     */
+    authorizationStatusWillChange: unknown;
+    /**
+     * A notification name indicating a user is eligible for a subscribe view.
+     */
+    eligibleForSubscribeView: unknown;
+    /**
+     * A notification name indicating MusicKit JS is loaded.
+     */
+    loaded: unknown;
+    /**
+     * A notification name indicating the player has obtained enough data for
+     * playback to start.
+     */
+    mediaCanPlay: unknown;
+    /**
+     * A notification name indicating that the currently-playing media item has
+     * changed.
+     */
+    mediaItemStateDidChange: MediaItem;
+    /**
+     * A notification name indicating the currently-playing media item is about
+     * to change.
+     */
+    mediaItemStateWillChange: MediaItem;
+    /**
+     * A notification name indicating that the player has thrown an error during
+     * playback.
+     */
+    mediaPlaybackError: { message: string };
+    /**
+     * A notification name indicating the media item's metadata has finished
+     * loading.
+     */
+    metadataDidChange: unknown;
+    /**
+     * A notification indicating the playback bit rate has changed.
+     */
+    playbackBitrateDidChange: { bitrate: number };
+    /**
+     * A notification name indicating the current playback duration changed.
+     */
+    playbackDurationDidChange: unknown;
+    /**
+     * Buffering progress changed.
+     */
+    bufferedProgressDidChange: { progress: number };
+    /**
+     * A notification name indicating the current playback progress changed.
+     */
+    playbackProgressDidChange: { progress: number };
+    /**
+     * A notification indicating the playback state has changed.
+     */
+    playbackStateDidChange: { nowPlayingItem: MusicKit.MediaItem; oldState: PlaybackStates; state: PlaybackStates };
+    /**
+     * A notification indicating the playback state is about to be changed.
+     */
+    playbackStateWillChange: { nowPlayingItem: MusicKit.MediaItem; oldState: PlaybackStates; state: PlaybackStates };
+    /**
+     * A notification indicating that a playback target's availability has changed.
+     */
+    playbackTargetAvailableDidChange: unknown;
+    /**
+     * A notification name indicating the current playback time has changed.
+     */
+    playbackTimeDidChange: { currentPlaybackDuration: number; currentPlaybackTime: number; currentPlaybackTimeRemaining: number; };
+    /**
+     * A notification name indicating the player volume has changed.
+     */
+    playbackVolumeDidChange: unknown;
+    /**
+     * A notification name indicating the playback has started in another context
+     * on your domain, and the current player has stopped playback.
+     */
+    primaryPlayerDidChange: unknown;
+    /**
+     * A notification name indicating that the items in the current playback
+     * queue have changed.
+     */
+    queueItemsDidChange: MediaItem[];
+    /**
+     * A notification name indicating that the current queue position has changed.
+     */
+    queuePositionDidChange: { oldPosition: number; position: number };
+    /**
+     * A notification name indicating a change in the storefront country code.
+     */
+    storefrontCountryCodeDidChange: unknown;
+    /**
+     * A notification name indicating that the device's inferred storefront
+     * identifier changed.
+     */
+    storefrontIdentifierDidChange: unknown;
+    /**
+     * A notification name indicating the user token changed.
+     */
+    userTokenDidChange: { userToken: string };
+    /**
+     * A notification name indicating the currently playing item has changed.
+     */
+    nowPlayingItemDidChange: { item: MediaItem };
+    /**
+     * A notification name indicating if the repeat mode did change.
+     */
+    repeatModeDidChange: PlayerRepeatMode;
+    /**
+     * A notification name indicating if the shuffle mode did change.
+     */
+    shuffleModeDidChange: PlayerShuffleMode;
+    /**
+     * A notification name indicating if the autoplay mode did change.
+     */
+    autoplayEnabledDidChange: boolean;
+    /**
+     * The timed metadata changed.
+     */
+    timedMetadataDidChange: {
+      title: string;
+      performer: string;
+      album: string;
+      _adamId: string;
+      links: {[key: string]: { url: string; }}
+    };
+
+    /**
+     * A notification name indicating that the media element has been created.
+     */
+    mediaElementCreated: { mediaElement: unknown };
+  }
+}
---
+++ MusicKit.MediaItem.d.ts
@@ -0,0 +1,241 @@
+declare namespace MusicKit {
+  /**
+   * The options to use when defining a media item.
+   */
+  interface MediaItemOptions {
+    /**
+     * The attributes for the media item.
+     */
+    attributes?: MediaItemAttributes;
+    /**
+     * The identifier for the media item.
+     */
+    id?: string | undefined;
+    /**
+     * The type for the media item.
+     */
+    type?: any;
+  }
+
+  /**
+   * This property describes a media item.
+   */
+  type Descriptor = MediaItem | string;
+
+  /**
+   * This class represents a single media item.
+   */
+  class MediaItem {
+    href: any;
+
+    /**
+     * A constructor that creates a new media item from specified options.
+     */
+    constructor(options?: MediaItemOptions);
+
+    /**
+     * Prepares a media item for playback.
+     */
+    prepareToPlay(): Promise<void>;
+
+    /**
+     * A string of information about the album.
+     */
+    readonly albumInfo: string;
+    /**
+     * The id of the media item's album.
+     */
+    albumId?: string;
+    /**
+     * The title of the album.
+     */
+    readonly albumName: string;
+    /**
+     * The artist for a media item.
+     */
+    readonly artistName: string;
+    /**
+     * The artwork object for the media item.
+     */
+    readonly artwork: Artwork;
+    /**
+     * The artwork image for the media item.
+     */
+    readonly artworkURL: string;
+    /**
+     * The attributes object for the media item.
+     */
+    readonly attributes: any;
+    /**
+     * A string containing the content rating for the media item.
+     */
+    readonly contentRating: string;
+    /**
+     * The disc number where the media item appears.
+     */
+    readonly discNumber: number;
+    /**
+     * The identifier for the media item.
+     */
+    readonly id: string;
+    /**
+     * A string of common information about the media item.
+     */
+    readonly info: string;
+    /**
+     * A Boolean value that indicates whether the item has explicit lyrics or language.
+     */
+    readonly isExplicitItem: boolean;
+    /**
+     * A Boolean value that indicated whether the item is playable.
+     */
+    readonly isPlayable: boolean;
+    /**
+     * A Boolean value indicating whether the media item is prepared to play.
+     */
+    readonly isPreparedToPlay: boolean;
+    /**
+     * The ISRC (International Standard Recording Code) for a media item.
+     */
+    readonly isrc: string;
+    /**
+     * The playback duration of the media item.
+     */
+    readonly playbackDuration: number;
+    readonly playlistArtworkURL: string;
+    readonly playlistName: string;
+    /**
+     * The URL to an unencrypted preview of the media item.
+     */
+    readonly previewURL: string;
+    /**
+     * The release date of the media item.
+     */
+    readonly releaseDate?: Date | undefined;
+    /**
+     * The name of the media item.
+     */
+    readonly title: string;
+    /**
+     * The number of the media item in the album's track list.
+     */
+    readonly trackNumber: number;
+    /**
+     * The type of the media item.
+     */
+    type: any;
+    /**
+     * Relationships of the media item
+     */
+    readonly relationships: any;
+    /**
+     * A Boolean value that indicates whether the item is a cloud upload.
+     */
+    readonly isCloudUpload: boolean;
+    readonly _songId: string;
+    readonly songId: string;
+  }
+
+  /**
+   * This class is for attributes
+   */
+  class MediaItemAttributes {
+    /**
+     * A constructor that creates a new media item from specified options.
+     */
+    constructor(options?: MediaItemOptions);
+
+    /**
+     * The title of the album.
+     */
+    readonly albumName: string;
+    /**
+     * The artist for a media item.
+     */
+    readonly artistName: string;
+    /**
+     * The artwork object for the media item.
+     */
+    readonly artwork: Artwork;
+    /**
+     * The composer for a media item.
+     */
+    readonly composerName: string;
+    /**
+     * The current playback progress of the media item.
+     */
+    readonly currentPlaybackProgress: number;
+    /**
+     * The current playback time of the media item.
+     */
+    readonly currentPlaybackTime: number;
+    /**
+     * The disc number where the media item appears.
+     */
+    readonly discNumber: number;
+    /**
+     * The duration of the media item.
+     */
+    readonly durationInMillis: number;
+    /**
+     * The end time of the media item.
+     */
+    readonly endTime: number;
+    /**
+     * The genre of the media item.
+     */
+    readonly genreNames: string[];
+    /**
+     * The ISRC (International Standard Recording Code) for a media item.
+     */
+    readonly isrc: string;
+    /**
+     * The kind of the media item.
+     */
+    readonly kind: string;
+    /**
+     * The name of the media item.
+     */
+    readonly name: string;
+    /**
+     * The playback parameters for the media item.
+     */
+    readonly playParams: { [key: string]: any };
+    /**
+     * The previews for the media item.
+     */
+    readonly previews: any[];
+    /**
+     * The release date of the media item.
+     */
+    readonly releaseDate: string;
+    /**
+     * The remaining playback time of the media item.
+     */
+    readonly remainingTime: number;
+    /**
+     * The song id of the media item.
+     */
+    readonly songId: string;
+    /**
+     * The start time of the media item.
+     */
+    readonly startTime: number;
+    /**
+     * The status of the media item.
+     */
+    readonly status: keyof typeof MusicKit.PlaybackStates;
+    /**
+     * Is Playing
+     */
+    readonly isPlaying: boolean;
+    /**
+     * The track number of the media item.
+     */
+    readonly trackNumber: number;
+    /**
+     * The URL of the media item.
+     */
+    readonly url: { [key: string]: any };
+  }
+}
